bb64bac51a1d87b64847d8949f18ac32b3e3acd9
siwonKH <kanghyunkim313@gmail.com>
2024-08-09 23:01:12 +0900

    feat: disable redirection after one hour

--- background.js
+++ background.js
@@ -6,13 +6,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             }
         });
     } else if (message.action === "updateTab") {
-        chrome.storage.local.get(["last_url"], (result) => {
-            if (result.last_url) {
+        chrome.storage.local.get(['last_time', 'last_url'], (result) => {
+            const timeDifference = (new Date().getTime() - result.last_time) / 1000;
+
+            if (timeDifference <= 3600 && result.last_url) {
                 chrome.tabs.update(sender.tab.id, { url: result.last_url });
             }
-            chrome.storage.local.remove(["last_url", "last_url_tmp", "last_url_tmp_tmp"]);
         });
+        chrome.storage.local.remove(["last_url", "last_url_tmp"]);
     } else if (message.action === "saveLastUrlTmp") {
-        chrome.storage.local.set({ last_url_tmp: message.url });
+        chrome.storage.local.set({
+            last_url_tmp: message.url,
+            last_time: new Date().getTime()
+        });
     }
 });
